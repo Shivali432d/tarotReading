4d8553a4339bc27fcf9978b882d29d99e485bf12
Shivali432d <98483575+Shivali432d@users.noreply.github.com>
2022-02-10 12:34:58 +0530

    Add files via upload

---
+++ tarot.js
@@ -0,0 +1,12 @@
+
+
+$('.fun').click(function() {
+    $(this).html('<img src="pics/gif/anime1.gif"/>');
+});
+$('.fun').click(function() {
+    $(this).html('<img src="pics/gif/anime2.gif"/>');
+});
+$('.fun').click(function() {
+    $(this).html('<img src="pics/gif/anime3.gif"/>');
+});
+$(".")
